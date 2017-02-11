2de489ddc69ace3c910995e41085ca7086f8e2b9
LiuWei <987860319@qq.com>
2017-02-11 16:43:27 +0800

    complete list/dlist.c

--- list/dlist.c
+++ list/dlist.c
@@ -26,18 +26,91 @@ void dlist_destroy(DList *list)
 	memset(list, 0, sizeof(DList));
 }
 
+/**
+ * 将元素插入由list指定的双向链表中element元素之后。如果element为NULL。
+ * 则将元素插入到链表的开始位置。新的元素包含一个指向data的指针，因此只要
+ * 该元素仍在链表中，data所引用的内存空间就应该保持合法。由调用者负责管理
+ * data所引用的存储空间。
+ * @param  list    要操作的链表
+ * @param  element 指定的节点
+ * @param  data    要插入的元素
+ * @return         成功返回0，失败返回-1
+ */
 int dlist_ins_next(DList *list, DListElmt *element, const void *data)
 {
 	DListElmt *new_element;
 
-	if (element == NULL && dlist_size(list) != 0)
+	if ((new_element = (DListElmt *)malloc(sizeof(DListElmt))) == NULL)
 		return -1;
+
+	new_element->data = (void *)data;
+	if (element == NULL) {
+		if (dlist_size(list) == 0) {
+			new_element->next = NULL;
+			new_element->prev = NULL;
+			list->head = new_element;
+			list->tail = new_element;
+		} else {
+			new_element->next = list->head;
+			new_element->prev = NULL;
+			list->head->prev = new_element;
+			list->head = new_element;
+		}
+	} else {
+		new_element->next = element->next;
+		new_element->prev = element;
+
+		if (element->next == NULL)
+			list->tail = new_element;
+		else
+			element->next->prev = new_element;
+
+		element->next = new_element;
+	}
+	list->size++;
 
 	return 0;
 }
 
+/**
+ * 将元素插入由list指定的双向链表中element元素之前。如果element为NULL。
+ * 则将元素插入到链表的开始位置。新的元素包含一个指向data的指针，因此只要
+ * 该元素仍在链表中，data所引用的内存空间就应该保持合法。由调用者负责管理
+ * data所引用的存储空间。
+ * @param  list    要操作的链表
+ * @param  element 指定的节点
+ * @param  data    要插入的元素
+ * @return         成功返回0，失败返回-1
+ */
 int dlist_ins_prev(DList *list, DListElmt *element, const void *data)
 {
+	DListElmt *new_element;
+
+	if ((new_element = (DListElmt *)malloc(sizeof(DListElmt))) == NULL)
+		return -1;
+
+	if (element == NULL) {
+		if (dlist_size(list) == 0) {
+			list->head = new_element;
+			list->tail = new_element;
+			new_element->next = NULL;
+			new_element->prev = NULL;
+		} else {
+			new_element->next = list->head;
+			new_element->prev = NULL;
+			list->head->prev = new_element;
+			list->head = new_element;
+		}
+	} else {
+		new_element->next = element;
+		new_element->prev = element->prev;
+		if (element->prev == NULL)
+			list->head = new_element;
+		else
+			element->prev->next = new_element;
+		element->prev = new_element;
+	}
+
 	return 0;
 }
 
